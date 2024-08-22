55c46f4f1862615f73d05885bfead6edd9432de7
Pallas <mickyoshae@gmail.com>
2024-08-22 16:53:54 -0400

    Added .gitignore, added funcionality for splitting and handling splitting of asteroids.

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
 
@@ -13,3 +14,21 @@ class Asteroid(CircleShape):
     
     def draw(self, screen):
         pygame.draw.circle(screen, (128, 128, 128), self.position, self.radius, 2)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        
+        new_velocity1 = self.velocity.rotate(random_angle)
+        new_velocity2 = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = new_velocity1 * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = new_velocity2 * 1.2
